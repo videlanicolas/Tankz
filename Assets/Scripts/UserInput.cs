c746e89003ee4b5ba9f9e1e0140243c8eed04f19
videlanicolas <videlanicolas@gmail.com>
2020-01-27 18:49:24 +1300

    Add bullet physics.

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    [Range(0.0f, 10.0f)]
+    public float forceMultiplier = 1.0f;
+
     private GameObject player;
     private GameObject player1Barrel;
     private GameObject player1Pivot;
+    private float bulletTTL = 10.0f;
 
     public float lowLimit = -45f, highLimit = 0f;
     // Start is called before the first frame update
@@ -22,8 +29,10 @@ public class UserInput : MonoBehaviour
     {
         float horizontalButton = Input.GetAxis("Horizontal");
         float verticalButton = Input.GetAxis("Vertical");
-        Debug.Log("verticalButton: " + verticalButton);
-        Debug.Log("player1Barrel.transform.rotation.z: " + player1Barrel.transform.rotation.z);
+        bool fire1 = Input.GetButtonDown("Fire1");
+        //Debug.Log("verticalButton: " + verticalButton);
+        //Debug.Log("fire1: " + fire1);
+        //Debug.Log("player1Barrel.transform.rotation.z: " + player1Barrel.transform.rotation.z);
 
         player.GetComponent<Transform>().Translate(horizontalButton * 0.1f, 0, 0);
         player1Barrel.GetComponent<Transform>().RotateAround(
@@ -31,6 +40,14 @@ public class UserInput : MonoBehaviour
                     Vector3.forward,
                     angle: verticalButton);
 
-
+        if (fire1) {
+            Debug.Log("Fire!");
+            GameObject bullet;
+            bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(
+                forceMultiplier * (spawnPoint.position - player1Pivot.transform.position), 
+                ForceMode.Impulse);
+            Destroy(bullet, bulletTTL);
+        }
     }
 }
