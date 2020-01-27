de4a011474e7e9155036db92b1efc7e779c466d3
videlanicolas <videlanicolas@gmail.com>
2020-01-27 23:21:42 +1300

    Refactor code for Tank into a separate class. Implement increasing power on fire.

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : ScriptableObject
+{
+    private GameObject hull, barrel;
+    private Transform pivot, spawnPoint;
+
+    public Tank(GameObject gameObject, GameObject barrel, Transform pivot, Transform spawnPoint) {
+        if (gameObject == null) this.hull = GameObject.FindGameObjectWithTag("Player");
+        else this.hull = gameObject;
+
+        if (pivot == null) this.pivot = GameObject.FindGameObjectWithTag("Player1Pivot").transform;
+        else this.pivot = pivot;
+
+        if (spawnPoint == null) this.spawnPoint = GameObject.FindGameObjectWithTag("Player1Spawn").transform;
+        else this.spawnPoint = spawnPoint;
+
+        if (barrel == null) this.barrel = GameObject.FindGameObjectWithTag("Player1Barrel");
+        else this.barrel = barrel;
+    }
+
+    public void Move(float translate) {
+        this.hull.GetComponent<Transform>().Translate(translate, 0, 0);
+    }
+
+    public void Aim(float speed) {
+        this.barrel.GetComponent<Transform>().RotateAround(
+                this.pivot.position,
+                Vector3.forward,
+                angle: speed);
+    }
+
+    public GameObject Fire(GameObject bulletPrefab, float power) {
+        GameObject bullet;
+        bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(
+            power * (this.spawnPoint.position - this.pivot.position),
+            ForceMode.Impulse);
+        return bullet;
+    }
+}
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+
+    [Header("Player 1")]
+    public GameObject tankPrefab;
+    public GameObject tankBarrel;
+    public GameObject tankPivot;
+    [Header("Bullet")]
     public GameObject bulletPrefab;
-    public Transform spawnPoint;
+    public Transform bulletSpawn;
+    [Range(0.1f, 2.0f)]
+    public float speed = 0.1f;
+    public float bulletTimeToLive = 10.0f;
+    [Range(1.0f, 10.0f)]
+    public float maxPower = 1.0f;
+    [Space]
 
-    [Range(0.0f, 10.0f)]
-    public float forceMultiplier = 1.0f;
+    private Tank player1;
+    private bool charge = false;
+    private float power = 1.0f;
 
-    private GameObject player;
-    private GameObject player1Barrel;
-    private GameObject player1Pivot;
-    private float bulletTTL = 10.0f;
-
-    public float lowLimit = -45f, highLimit = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        player1Barrel = GameObject.FindGameObjectWithTag("Player1Barrel");
-        player1Pivot = GameObject.FindGameObjectWithTag("Player1Pivot");
+        this.player1 = new Tank(this.tankPrefab, this.tankBarrel, this.tankPivot.transform, this.bulletSpawn);
     }
 
     // Update is called once per frame
@@ -29,25 +34,28 @@ public class UserInput : MonoBehaviour
     {
         float horizontalButton = Input.GetAxis("Horizontal");
         float verticalButton = Input.GetAxis("Vertical");
-        bool fire1 = Input.GetButtonDown("Fire1");
+        bool fire1Down = Input.GetButtonDown("Fire1"),
+             fire1Up = Input.GetButtonUp("Fire1");
         //Debug.Log("verticalButton: " + verticalButton);
         //Debug.Log("fire1: " + fire1);
         //Debug.Log("player1Barrel.transform.rotation.z: " + player1Barrel.transform.rotation.z);
 
-        player.GetComponent<Transform>().Translate(horizontalButton * 0.1f, 0, 0);
-        player1Barrel.GetComponent<Transform>().RotateAround(
-                    player1Pivot.transform.position,
-                    Vector3.forward,
-                    angle: verticalButton);
+        this.player1.Move(horizontalButton * 0.1f);
+        this.player1.Aim(verticalButton);
 
-        if (fire1) {
-            Debug.Log("Fire!");
-            GameObject bullet;
-            bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().AddForce(
-                forceMultiplier * (spawnPoint.position - player1Pivot.transform.position), 
-                ForceMode.Impulse);
-            Destroy(bullet, bulletTTL);
+        if (fire1Down) {
+            charge = true;
+        }
+
+        if (charge) {
+            power += Time.deltaTime;
+            Mathf.Clamp(power, 0.0f, this.maxPower);
+        }
+
+        if (fire1Up) {
+            Debug.Log("power: " + power);
+            charge = false;
+            Destroy(this.player1.Fire(this.bulletPrefab, power), this.bulletTimeToLive);
         }
     }
 }
